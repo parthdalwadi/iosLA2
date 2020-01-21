95ab28e1a70fe40b9ebaa4f19ea93370ee05971f
Parth Dalwadi <pbdalwadi@gmail.com>
2020-01-21 11:56:19 -0500

    - assignment basic finished

---
+++ Parth_C0766346_LA2/controllers/TaskListTVC.swift
@@ -0,0 +1,322 @@
+//
+//  TaskListTVC.swift
+//  Parth_C0766346_LA2
+//
+//  Created by Parth Dalwadi on 2020-01-19.
+//  Copyright © 2020 Parth Dalwadi. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TaskListTVC: UITableViewController, UISearchBarDelegate {
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    var allTasks: [NSManagedObject]?
+    var showAlert = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAllData()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+         self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        let req = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+        if !searchText.isEmpty{
+            req.predicate = NSPredicate(format: "title contains[c] %@" , searchText)}
+        do {
+         
+            self.allTasks = try self.context.fetch(req) as! [NSManagedObject]
+            self.tableView.reloadData()
+            
+         }catch {
+         print("error occured in fetching")
+         }
+        
+    }
+    
+    
+    
+    
+    // MARK: - Table view data source
+
+    @IBAction func unwindToList(_ unwindSegue: UIStoryboardSegue) {
+           let s = unwindSegue.source as! ViewController
+        
+        if (s.isNewTask && isTaskExist(title: s.titleL.text!)){
+            
+            // alert
+            showAlert = true
+           
+            
+            
+        }else{
+            s.saveTask()
+            showAlert = false
+            loadAllData()
+            tableView.reloadData()
+        }
+        
+        
+           // Use data from the view controller which initiated the unwind segue
+       }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+        if showAlert{
+            
+             okAlert(msg: "Task with the same name already exists")
+            
+        }
+        
+    }
+    
+    func okAlert(msg: String){
+        
+        let ac = UIAlertController(title: msg, message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action) in
+            self.showAlert = false
+        }))
+        
+        self.present(ac, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    func isTaskExist(title: String)->Bool{
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+        fetchRequest.predicate = NSPredicate(format: "title=%@" , title)
+        
+        var results: [NSManagedObject]
+        do {
+            results = try context.fetch(fetchRequest) as! [NSManagedObject]
+            
+          
+            }catch {
+            print("error occured in fetching")
+            return true
+            }
+        
+        return (!results.isEmpty)
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return allTasks?.count ?? 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell"){
+            
+            cell.textLabel?.text = allTasks![indexPath.row].value(forKey: "title") as! String
+
+            let Ndays = allTasks![indexPath.row].value(forKey: "daysNeeded") as! Int
+            let Adays = allTasks![indexPath.row].value(forKey: "daysAdded") as! Int
+            
+            cell.detailTextLabel?.text = (Ndays == Adays) ? "☑️" :"\(Adays)/\(Ndays)"
+            cell.backgroundColor =  (Ndays == Adays) ? UIColor.green: UIColor.magenta
+            
+            return cell }
+        
+        return UITableViewCell()
+    }
+    
+
+
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            
+            print("something added")
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }*/
+    
+
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let DeleteAction = UIContextualAction(style: .destructive, title: "Remove Task") { (action, view, success) in
+            
+            self.context.delete(self.allTasks![indexPath.row])
+            self.allTasks?.remove(at: indexPath.row)
+            
+            do{
+                
+                try self.context.save()
+            }catch{
+                print("unable to delete")
+            }
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            self.tableView.reloadData()
+            
+        }
+        
+        
+        let addDay = UIContextualAction(style: .normal , title: "Add a Day") { (action, view, success) in
+ 
+            let days = self.allTasks![indexPath.row].value(forKey: "daysAdded") as! Int
+            
+            self.allTasks![indexPath.row].setValue(days + 1, forKey: "daysAdded")
+            
+            do{
+                
+                try self.context.save()
+               
+            }catch{
+                print("error")
+            }
+            
+            self.tableView.reloadData()
+            
+        }
+        return UISwipeActionsConfiguration(actions: [DeleteAction, addDay])
+    }
+    
+       
+    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        // re arrange in core data
+     
+        let fromV = allTasks![fromIndexPath.row]
+        let toV = allTasks![to.row]
+        
+        allTasks![fromIndexPath.row] = toV
+        allTasks![to.row] = fromV
+        
+        tableView.reloadData()
+        
+        do{
+            try context.save()
+        
+        }catch{
+            print("unable to rearrange")
+        }
+    }
+
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        if let taskVC = segue.destination as? ViewController{
+            //taskVC.d_tvc = self
+            if let btn = sender as? UIBarButtonItem {
+                taskVC.isNewTask = true
+            }
+            
+            if let task_Cell = sender as? UITableViewCell {
+                taskVC.isNewTask = false
+                taskVC.taskName = task_Cell.textLabel?.text
+                
+            }
+            
+            
+            
+        }
+    }
+    
+
+    
+    // function that returns all data
+    func loadAllData(){
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+        
+        do {
+            allTasks = try context.fetch(fetchRequest) as! [NSManagedObject]
+          
+            }catch {
+            print("error occured in fetching")
+            }
+    }
+    
+    
+    @IBAction func sort(_ sender: UIBarButtonItem) {
+        
+        var sortKey = ""
+        let ac = UIAlertController(title: "Sort By:", message: "Please select a key", preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "Title", style: .default, handler: { (action) in
+            sortKey = "title"
+            self.performSort(sortKey, sender.title == "up")
+            
+        }))
+
+        ac.addAction(UIAlertAction(title: "Created On", style: .default, handler: { (action) in
+            sortKey = "createdOn"
+            self.performSort(sortKey, sender.title == "up")
+            
+            
+        }))
+
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        self.present(ac, animated: true, completion: nil)
+        
+        
+    
+    }
+    
+    
+    func performSort(_ sortKey: String, _ isAccending: Bool){
+        
+        
+        let req = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+        
+        req.sortDescriptors = [NSSortDescriptor(key: sortKey, ascending: isAccending)]
+        do {
+         
+            self.allTasks = try self.context.fetch(req) as! [NSManagedObject]
+            self.tableView.reloadData()
+            
+         }catch {
+         print("error occured in fetching")
+         }
+    }
+
+}
---
+++ Parth_C0766346_LA2/controllers/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  Parth_C0766346_LA2
+//
+//  Created by Parth Dalwadi on 2020-01-19.
+//  Copyright © 2020 Parth Dalwadi. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var titleL: UITextField!
+    @IBOutlet weak var detailL: UITextView!
+    @IBOutlet weak var daysNeededL: UITextField!
+   
+    @IBOutlet weak var added: UILabel!
+    @IBOutlet weak var timeStampL: UILabel!
+    @IBOutlet weak var created_on_L: UILabel!
+    
+    //var d_tvc: TaskListTVC?
+    var isNewTask = false
+    var taskName: String?
+    var currTask: NSManagedObject?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set context for accessing task entity
+        if !isNewTask {
+            timeStampL.isHidden = false
+            created_on_L.isHidden = false
+            displayTaskDetails(taskTitle: taskName!)}
+        else{
+            timeStampL.isHidden = true
+            created_on_L.isHidden = true
+            titleL.isEnabled = true
+        }
+       
+        
+    }
+    
+    
+    
+
+    
+    
+    func saveTask() {
+        
+        if isNewTask {
+            currTask = NSEntityDescription.insertNewObject(forEntityName: "Task", into: context)
+            
+        }
+        
+        
+                currTask!.setValue(titleL.text, forKey: "title")
+                currTask!.setValue(detailL.text, forKey: "details")
+                currTask!.setValue(isNewTask ? NSDate() : (currTask?.value(forKey: "createdOn")) , forKey: "createdOn")
+                currTask!.setValue(Int(daysNeededL.text!) ?? 0, forKey: "daysNeeded")
+                currTask!.setValue(0, forKey: "daysAdded")
+
+        
+                  // save context
+                  do {
+                      try context.save()
+                    
+                  } catch {
+                      print(error)
+                  }
+        
+    }
+    
+    
+    
+    func displayTaskDetails(taskTitle: String){
+        
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+        fetchRequest.predicate = NSPredicate(format: "title=%@", taskTitle)
+        
+        do {
+            let results = try context.fetch(fetchRequest) as! [NSManagedObject]
+            
+            currTask = results[0]
+            
+            titleL.text = taskTitle
+            titleL.isEnabled = false
+            detailL.text = currTask!.value(forKey: "details") as! String
+            daysNeededL.text = "\(currTask!.value(forKey: "daysNeeded")!)"
+            timeStampL.text = "\((currTask!.value(forKey: "createdOn") as! NSDate).description.dropLast(5))"
+            
+            added.text = "Added Days: \((currTask!.value(forKey: "daysAdded")!))"
+            
+            }catch {
+            print("error occured in fetching")
+            }
+        
+        
+    }
+    
+    
+}
+
